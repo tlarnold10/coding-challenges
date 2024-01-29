455a509777694fde2c0e18957514ead7f2210809
tlarnold10 <tlarnold10@gmail.com>
2024-01-29 07:15:50 -0600

    finished up step 3, 4 and 5

--- uniqTool/uniqTool.go
+++ uniqTool/uniqTool.go
@@ -4,28 +4,31 @@ import (
   "fmt"
   "os"
   "log"
-  // "strconv"
+  "strconv"
   "strings"
   "bufio"
 )
 
-func main() {
-  var outputFilename string
-  var data []byte
-  var err error
-  var filename string
+var outputFilename string
+var data []byte
+var err error
+var filename string
 
-  args := os.Args[1:]
-  initialArg := args[0]
-  
-
-  if (initialArg == "-c") {
-    
+func postResults(results string) {
+   if (outputFilename != "") {
+    fmt.Println("Writing to file " + outputFilename)
+    err = os.WriteFile(outputFilename, []byte(results), 0666)
+    if (err != nil) {
+      log.Fatal("Error in writing to file", err)
+    }
   } else {
-    filename = args[0]
-  }
-  
-    if (len(args) == 2) {
+    fmt.Println(results)
+  } 
+}
+
+func standardMode(args []string) {
+  filename = args[0] 
+  if (len(args) == 2) {
     outputFilename = args[1]
   }
 
@@ -57,14 +60,162 @@ func main() {
       previousLine = dataString[i]
     }
   }
+  postResults(results)
+}
 
-  if (outputFilename != "") {
-    err = os.WriteFile(outputFilename, []byte(results), 0666)
+func countMode(args []string) {
+  filename = args[1] 
+  if (len(args) == 3) {
+    outputFilename = args[2]
+  }
+
+  if (filename != "-") {
+    data, err = os.ReadFile(filename)
     if (err != nil) {
-      log.Fatal("Error in writing to file", err)
+      log.Fatal("Error reading file", err)
     }
   } else {
-    fmt.Println("hello?")
-    fmt.Println(results)
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+      data = append(data, scanner.Bytes()...)
+      data = append(data, []byte("\n")...)
+    }
+  }
+
+  countResults := make(map[string]int)
+  var results string
+  var previousLine string
+  dataString := strings.Split(string(data), "\n")
+  
+  for i := 0; i < len(dataString); i++ {
+    if (previousLine == "") {
+      previousLine = dataString[i]
+      countResults[dataString[i]] = 1
+    } else {
+      if (dataString[i] != previousLine) {
+        countResults[dataString[i]] = 1
+      } else {
+        countResults[dataString[i]] = countResults[dataString[i]] + 1
+      }
+      previousLine = dataString[i]
+    }
+  }
+
+  for key, value := range countResults {
+    if (key != "") {
+      results = results + strconv.Itoa(value) + "\t" + key + "\n"
+    }
+  }
+  postResults(results)
+}
+
+func repeatMode(args []string) {
+  filename = args[1] 
+  if (len(args) == 3) {
+    outputFilename = args[2]
+  }
+
+  if (filename != "-") {
+    data, err = os.ReadFile(filename)
+    if (err != nil) {
+      log.Fatal("Error reading file", err)
+    }
+  } else {
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+      data = append(data, scanner.Bytes()...)
+      data = append(data, []byte("\n")...)
+    }
+  }
+
+  countResults := make(map[string]int)
+  var results string
+  var previousLine string
+  dataString := strings.Split(string(data), "\n")
+  
+  for i := 0; i < len(dataString); i++ {
+    if (previousLine == "") {
+      previousLine = dataString[i]
+      countResults[dataString[i]] = 1
+    } else {
+      if (dataString[i] != previousLine) {
+        countResults[dataString[i]] = 1
+      } else {
+        countResults[dataString[i]] = countResults[dataString[i]] + 1
+      }
+      previousLine = dataString[i]
+    }
+  }
+
+  for key, value := range countResults {
+    if (key != "" && value > 1) {
+      results = results + key + "\n"
+    }
+  }
+  postResults(results)
+}
+
+func uniqueMode(args []string) {
+  filename = args[1] 
+  if (len(args) == 3) {
+    outputFilename = args[2]
+  }
+
+  if (filename != "-") {
+    data, err = os.ReadFile(filename)
+    if (err != nil) {
+      log.Fatal("Error reading file", err)
+    }
+  } else {
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+      data = append(data, scanner.Bytes()...)
+      data = append(data, []byte("\n")...)
+    }
+  }
+
+  countResults := make(map[string]int)
+  var results string
+  var previousLine string
+  dataString := strings.Split(string(data), "\n")
+  
+  for i := 0; i < len(dataString); i++ {
+    if (previousLine == "") {
+      previousLine = dataString[i]
+      countResults[dataString[i]] = 1
+    } else {
+      if (dataString[i] != previousLine) {
+        countResults[dataString[i]] = 1
+      } else {
+        countResults[dataString[i]] = countResults[dataString[i]] + 1
+      }
+      previousLine = dataString[i]
+    }
+  }
+
+  for key, value := range countResults {
+    if (key != "" && value == 1) {
+      results = results + key + "\n"
+    }
+  }
+  postResults(results)
+}
+
+func main() {
+  args := os.Args[1:]
+  initialArg := args[0]
+  
+  if (initialArg == "-c") {
+    fmt.Println("Running in count mode")
+    countMode(args)
+  } else if (initialArg == "-d") {
+    fmt.Println("Running in repeated mode")
+    repeatMode(args)
+  } else if (initialArg == "-u") {
+    fmt.Println("Running in unique mode")
+    uniqueMode(args)
+  } else {
+    fmt.Println("Running in standard mode")
+    standardMode(args)
   }
 }
