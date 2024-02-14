faa1e89e2a3bf662b66296f41c170483fe3035b6
tlarnold10 <tlarnold10@gmail.com>
2024-02-14 17:24:52 -0600

    finished all steps from discord bot coding challenge

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ This repo is used to store my coding challenges from https://codingchallenges.fy
     - Written in Go.
     - To run the tool, naviage to the uniqTool directory and run `uniqTool`. You can then specify the mode as an optional argument (-c to add a line count for each corresponding line, -d to just display only repeated lines, -u to display only unique lines, or nothing in order to run in standard mode, which will not display any duplicated lines), input file (required argument either the file name or `-` to read from the standard input), and output file (optional argument).
 4. Build your own Discord bot: https://codingchallenges.fyi/challenges/challenge-discord
+    - Written in Go.
+    - To run the tool, navigate to the disdaccbot directory and run `disdaccbot` and provide the discord bot token after the `-t` flag.
+    - Once the bot is running, you should be able to see a log stating the bot is now running. The bot is currently connected to my own personal discord server. But I could go to my discord server and run the following commands:
+        1. `Hello`: which would result in the bot responding with `Hello`
+        2. `!quote`: will grab a random quote and respond with that quote and the author.
+        3. `!challenge`: will grab a random challenge from the Coding Challenge json file.
+        4. `!list`: will grab and list all the Coding Challenges from the json file.
+        5. `!add`: will take a url of a Coding Challenge, confirm it is a valid url and then respond with the title from the html page.
+    - Next steps would be to add the functionality to run on something other than my PC as well as actually add the coding challenge to my local json.
--- disdaccbot/disdaccbot.go
+++ disdaccbot/disdaccbot.go
@@ -4,12 +4,15 @@ import (
   "fmt"
   "flag"
   "os"
+  "strings"
   "github.com/bwmarrin/discordgo"
   "os/signal"
   "syscall"
   "io/ioutil"
   "net/http"
   "encoding/json"
+  "math/rand"
+  "golang.org/x/net/html"
 )
 
 type Quote struct {
@@ -18,11 +21,130 @@ type Quote struct {
   Author  string
 }
 
+type Challenge struct {
+  Name  string `json:"name"`
+  Url   string `json:"url"`
+}
+
+type Challenges struct {
+  Challenges []Challenge `json:"challenges"`
+}
+
 var TOKEN string
 
 func init() {
   flag.StringVar(&TOKEN, "t", "", "Bot Token")
   flag.Parse()
+}
+
+// If the user just types "Hello"
+func sayHi(s *discordgo.Session, m *discordgo.MessageCreate, author string) {
+  s.ChannelMessageSend(m.ChannelID, "Hello " + author)
+}
+
+// If the user types "!quote"
+func randomQuote(s *discordgo.Session, m *discordgo.MessageCreate) {
+  resp, err := http.Get("https://dummyjson.com/quotes/random")
+  if err != nil {
+    fmt.Println("Error in getting dummyjson quote: ", err)
+    return
+  }
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Error in parsing response from dummyjson quote: ", err)
+    return
+  }
+  var randomQuote Quote
+  err = json.Unmarshal(body, &randomQuote)
+  if err != nil {
+    fmt.Println("Error in parsing json to quote: ", err)
+    return
+  }
+  newMessage := "\"" + randomQuote.Quote + "\" - " + randomQuote.Author
+  s.ChannelMessageSend(m.ChannelID, newMessage)
+}
+
+// if the user types "!challenge"
+func randomChallenge(s *discordgo.Session, m *discordgo.MessageCreate) {
+  jsonFile, err := os.Open("challenges.json")
+  if err != nil {
+    fmt.Println("Error reading challenges.json file")
+    return
+  }
+  var challenges Challenges
+  challengesData, _ := ioutil.ReadAll(jsonFile)
+  err = json.Unmarshal(challengesData, &challenges)
+  if err != nil {
+    fmt.Println("Error in parsing json to a []Challenge")
+    return
+  }
+  max := len(challenges.Challenges)
+  random := rand.Intn(max) - 1
+  randomPick := challenges.Challenges[random]
+  newMessage := randomPick.Name + ": " + randomPick.Url
+  s.ChannelMessageSend(m.ChannelID, newMessage)
+}
+
+// if the user types "!list"
+func getAllChallenges(s *discordgo.Session, m *discordgo.MessageCreate) {
+  jsonFile, err := os.Open("challenges.json")
+  if err != nil {
+    fmt.Println("Error reading challenges.json file")
+    return
+  }
+  var challenges Challenges
+  challengesData, _ := ioutil.ReadAll(jsonFile)
+  err = json.Unmarshal(challengesData, &challenges)
+  if err != nil {
+    fmt.Println("Error in parsing json to a []Challenge")
+    return
+  }
+  newMessage := ""
+  max := len(challenges.Challenges)
+  for i := 0; i < max; i++ {
+    if (i == max - 1) {
+      newMessage = newMessage + challenges.Challenges[i].Name + ": " + challenges.Challenges[i].Url
+    } else {
+      newMessage = newMessage + challenges.Challenges[i].Name + ": " + challenges.Challenges[i].Url + "\n"
+    }
+  }
+  s.ChannelMessageSend(m.ChannelID, newMessage)
+}
+
+// if the user types "!add"
+func addChallenge(s *discordgo.Session, m *discordgo.MessageCreate, url string) {
+  resp, err := http.Get(url)
+  if err != nil {
+    newMessage := "Unable to add: " + url + " please check it is a valid Coding Challenge url"
+    s.ChannelMessageSend(m.ChannelID, newMessage)
+    return
+  }
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Error in parsing response from dummyjson quote: ", err)
+    return
+  }
+  doc, err := html.Parse(strings.NewReader(string(body)))
+  if err != nil {
+    fmt.Println("Error in parsing body: ", err)
+    return
+  }
+  nextPass := false
+  newMessage := ""
+  var f func(*html.Node, bool)
+  f = func(n *html.Node, nextPass bool) {
+    if n.Type == html.ElementNode && n.Data == "title" {
+      nextPass = true
+    } else if (nextPass) {
+      newMessage = "Added: " + n.Data + ": " + url
+      nextPass = false
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+      f(c, nextPass)
+    }
+  }
+  f(doc, nextPass)
+  s.ChannelMessageSend(m.ChannelID, newMessage)
 }
 
 func main() {
@@ -49,35 +171,28 @@ func main() {
   dis.Close()
 }
 
+// listens for messages from discord and responds
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-  messageAuthor := m.Author
-
   if m.Author.ID == s.State.User.ID {
     return
   }
 
   if (m.Content == "Hello") {
-    s.ChannelMessageSend(m.ChannelID, "Hello " + messageAuthor.Username)
+    sayHi(s, m, m.Author.Username) 
   } else if (m.Content == "!quote") {
-    resp, err := http.Get("https://dummyjson.com/quotes/random")
-    if err != nil {
-      fmt.Println("Error in getting dummyjson quote: ", err)
-      return
-    }
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-      fmt.Println("Error in parsing response from dummyjson quote: ", err)
-      return
-    }
-    var randomQuote Quote
-    err = json.Unmarshal(body, &randomQuote)
-    if err != nil {
-      fmt.Println("Error in parsing json to quote: ", err)
-      return
-    }
-    newMessage := "\"" + randomQuote.Quote + "\" - " + randomQuote.Author
-    s.ChannelMessageSend(m.ChannelID, newMessage)
+    randomQuote(s, m)
   } else if (m.Content == "!challenge") {
-
+    randomChallenge(s, m)
+  } else if (m.Content == "!list") {
+    getAllChallenges(s, m)
+  } else if (m.Content[:4] == "!add") {
+    if (len(strings.Split(m.Content, " ")) < 2) {
+      missingUrl := "Please provide a url with the \"!add\" command"
+      fmt.Println(missingUrl)
+      s.ChannelMessageSend(m.ChannelID, missingUrl)
+      return
+    }
+    newUrl := strings.Split(m.Content, " ")[1]
+    addChallenge(s, m, newUrl)
   }
 }
