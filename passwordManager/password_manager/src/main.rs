70947dd522c384ad6f9eef56f13a552b9cfdaee3
tlarnold10 <tlarnold10@gmail.com>
2024-08-13 09:50:07 -0500

    able to read and write to the sqlite database

--- passwordManager/password_manager/src/main.rs
+++ passwordManager/password_manager/src/main.rs
@@ -6,20 +6,20 @@ use axum::{
     Json,
     Router,
     response::{ IntoResponse, Html },
-    extract::Form
+    extract::{ Form, Extension }
 };
 // use serde::{ Deserialize };
 use templates::{ Index, PasswordItem, PasswordItemSnippet };
 use askama::Template;
 use std::env;
-use rusqlite::{ Connection, Result };
+use rusqlite::{ Connection, Result, Statement };
 use lazy_static::lazy_static;
 use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
     // connecting to sqlite
-    let conn = Connection::open("passwords.db");
+    // let conn = Connection::open("passwords.db");
 
     let app = Router::new()
         .route("/", get(index))
@@ -38,6 +38,7 @@ async fn main() {
 // persist throughout the life of the program
 lazy_static! {
     static ref DATA: Mutex<Vec<PasswordItem>> = Mutex::new(Vec::new());
+    static ref CONN: Mutex<Connection> = Mutex::new(Connection::open("passwords.db").unwrap());
 }
 
 fn print_vec(items: &[PasswordItem]) -> () {
@@ -50,17 +51,26 @@ fn print_vec(items: &[PasswordItem]) -> () {
 async fn index() -> impl IntoResponse {
     let mut vec: Vec<PasswordItem> = Vec::new();
     let mut passwords = DATA.lock().await;
-    passwords.push({ PasswordItem {
-        password: "test".to_string(),
-        username: "more testing".to_string(),
-        account: "thing".to_string()
-    }});
+    let query = "SELECT account, username, password FROM passwords";
+    let conn = CONN.lock().await;
+    let mut stmt = conn.prepare(&query).unwrap();
+    let passwords_iter = stmt.query_map([], |row| {
+        Ok( PasswordItem {
+            account: row.get(0)?,
+            username: row.get(1)?,
+            password: row.get(2)?,
+        })
+    });
 
-    passwords.push({ PasswordItem {
-        account: "another one".to_string(),
-        password: "wouldn't you like to know!".to_string(),
-        username: "something".to_string()
-    }});
+    for password in passwords_iter.unwrap() {
+        let temp_password = password.unwrap();
+        passwords.push({ PasswordItem {
+            account: temp_password.account,
+            username: temp_password.username,
+            password: temp_password.password
+        }})
+    } 
+
     let template = Index { name: "trevor".to_string(), passwords: passwords.to_vec() };
     match template.render() {
         Ok(html) => Html(html).into_response(),
@@ -84,6 +94,9 @@ async fn create_password(Form(payload): Form<PasswordItem>) -> impl IntoResponse
     };
     passwords.push(password.clone());
     print_vec(&passwords);
+    let query = format!("INSERT INTO passwords VALUES ('{}', '{}', '{}')", 
+                                    password.account.clone(), password.username.clone(), password.password.clone());
+    CONN.lock().await.execute(&query, ()).unwrap();
     let added_item = format!("Account: {} Password: {} Username: {}", password.account, password.password, password.username);
     // return Html(&added_item).into_response();
     let template = PasswordItemSnippet { snippet: added_item };
